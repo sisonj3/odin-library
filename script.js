bfa661f286c8c42d9f1977e2a786eac99db91706
sisonj <98570174+sisonj3@users.noreply.github.com>
2023-10-05 14:01:27 -0700

    Added book constructor, function to add book to the library, and a function to display the book on the page

---
+++ script.js
@@ -0,0 +1,51 @@
+let myLibrary = [];
+
+// Container that holds all book diplays
+const bookDisplays = document.querySelector('.books');
+
+// Book Constructor
+function Book (title, author, pages, hasRead) {
+    this.title = title; // String
+    this.author = author; // String
+    this.pages = pages; // Number
+    this.hasRead = hasRead; // Boolean
+}
+
+function addBookToLibrary(book) {
+    console.log(Book.prototype.isPrototypeOf(book));
+
+    if(Book.prototype.isPrototypeOf(book)){
+        myLibrary.push(book);
+    }
+}
+
+function displayBook(book) {
+    console.log(Book.prototype.isPrototypeOf(book));
+
+    // Dom Elements for creating book display
+    let bookDisplay = document.createElement('div');
+    let bookTitle = document.createElement('div');
+    let bookAuthor = document.createElement('div');
+    let bookPages = document.createElement('div');
+    let bookRead = document.createElement('div');
+
+    bookDisplay.classList.add('book-display');
+
+    // Set text for title, author, pages, and read
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    bookPages.textContent = book.pages;
+    bookRead.textContent = book.hasRead;
+
+    // Insert Elements into document
+    bookDisplay.appendChild(bookTitle);
+    bookDisplay.appendChild(bookAuthor);
+    bookDisplay.appendChild(bookPages);
+    bookDisplay.appendChild(bookRead);
+    bookDisplays.appendChild(bookDisplay);
+
+}
+
+let potter = new Book('Harry Potter', 'J. K. Rowling', 1000, false);
+
+displayBook(potter);
